9f79331527fcf888bfc9402b27ffa095b75acb26
James Sousa <jameswsousa.eng@gmail.com>
2021-01-07 19:33:57 -0400

    finishes get list

--- lib/api.dart
+++ lib/api.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:teste_ambar/git_repository.dart';
 
 class Api {
-  Future<List> getMap() async {
+  Future<List<GitRepository>> getList() async {
     http.Response response;
+    List<GitRepository> repositories = [];
     response = await http.get('https://api.github.com/repositories');
-    return json.decode(response.body);
+    json.decode(response.body).map((rep) {
+      repositories.add(GitRepository.fromJson(rep));
+    }).toList();
+    return repositories;
   }
 }
