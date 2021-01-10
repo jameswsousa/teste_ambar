ccaeeb67da9a19a32f6be234e4a9f73986b53d9e
James Sousa <jameswsousa.eng@gmail.com>
2021-01-10 13:28:32 -0400

    adds computed field and action method

--- lib/state/git_repository_store.dart
+++ lib/state/git_repository_store.dart
@@ -1,10 +1,13 @@
 import 'package:mobx/mobx.dart';
 import 'package:teste_ambar/data/api.dart';
 import 'package:teste_ambar/data/model/git_repository.dart';
+import 'package:teste_ambar/errors/exceptions.dart';
 
 part 'git_repository_store.g.dart';
 
 class GitRepositoryStore = _GitRepositoryStore with _$GitRepositoryStore;
+
+enum StoreState { initial, loading, loaded }
 
 abstract class _GitRepositoryStore with Store {
   final Api _api;
@@ -19,4 +22,30 @@ abstract class _GitRepositoryStore with Store {
 
   @observable
   String errorMessage;
+
+  @computed
+  StoreState get state {
+    if (_repsFuture == null || _repsFuture.status == FutureStatus.rejected) {
+      return StoreState.initial;
+    }
+
+    return _repsFuture.status == FutureStatus.pending
+        ? StoreState.loading
+        : StoreState.loaded;
+  }
+
+  @action
+  Future getList() async {
+    try {
+      errorMessage = null;
+      _repsFuture = ObservableFuture(_api.getList());
+      reps = await _repsFuture;
+    } on NoInternetException {
+      errorMessage = 'Verifique sua conexão com a internet';
+    } on DataException {
+      errorMessage = 'Erro ao obter dados';
+    } catch (e) {
+      errorMessage = 'Houve um erro de comunicação';
+    }
+  }
 }
